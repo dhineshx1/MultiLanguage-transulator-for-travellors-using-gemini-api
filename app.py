f22157bf1518a7d0c356b4db235d86c063c4db47
dhineshx1 <dhineshx1@gmail.com>
2023-12-27 15:56:20 +0530

    version 1

---
+++ app.py
@@ -0,0 +1,69 @@
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+import streamlit as st
+import os
+from PIL import Image
+
+
+import google.generativeai as genai
+
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load OpenAI model and get respones
+
+def get_gemini_response(input,image,prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input,image[0],prompt])
+    return response.text
+    
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="MULTILANGUAGE TRANSULATOR FOR TRAVELLORS")
+
+st.header("TRAVELLOR'S TRANSULATOR")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Tell me about the image")
+
+input_prompt = """
+               You are an expert in transulating and give information about the text in the picture. 
+                we will upload a image has street name board , hotel name ,  buliding name etc and 
+                you will have to answer any question based on the uploaded image and 
+                provide information about text in Multiple requested language else in english
+               """
+
+## If ask button is clicked
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response=get_gemini_response(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
